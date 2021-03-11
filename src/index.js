ee456f730f89384a18769b781fe567e45b019448
Artemy <artemij87work@gmail.com>
2021-03-11 18:05:29 +0300

    midied files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const AppHeader = () => {
+	return (
+		<h1>My Todo List</h1>
+	)
+}
+
+const SearchPanel = () => {
+	return (
+		<input placeholder="search" />
+	)
+}
+
+const TodoList = () => {
+	return (
+		<ul>
+		<li>Learn React</li>
+		<li>Build Awesome App</li>
+	</ul>
+	)
+}
+
+const App = () => {
+	return (
+		<div>
+			<AppHeader />
+			<SearchPanel />
+			<TodoList />
+		</div>
+	)
+}
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<App />, document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
